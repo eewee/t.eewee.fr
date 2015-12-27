28fb7b1e05cf67467a3a67ff89920381181d3c68
Michael <contact@eewee.fr>
2015-12-27 09:13:01 +0100

    Commit x

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Michael
- * Date: 04/06/2015
- * Time: 20:23
- */
-
 // TODO : test pour voir ...
-// test 123
-echo 'test<hr> fqsd';
-
-
-echo 'test...xxx...123';
-
+echo 'Test<hr>';
